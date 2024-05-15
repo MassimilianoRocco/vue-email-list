b357405f1178285f46168c39b19e39145e623f4a
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-05-15 15:31:13 +0200

    Added: Bonus 2

--- js/main.js
+++ js/main.js
@@ -4,28 +4,32 @@ createApp({
 
     data() {
         return {
-            listaMail: [],
+            listaMail:[],
             indirizzo: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            codiceRisposta:"",
+            datiRisposta:"",
+            inputUtente:"",
         }
     },
     methods: {
-       
+       addMails(input){
+        
+        for(let x = 0; x<input; x++){
+            axios.get(this.indirizzo).then((result) => {
+    
+                 codiceRisposta = `${result.status}/${result.statusText}`;
+                 datiRisposta = result.data;
+    
+                console.log("Ricevuta risposta", codiceRisposta, datiRisposta);
+    
+                this.listaMail.push(datiRisposta.response);     
+                });
+            }
+       }
     },
     mounted() {
-        let codiceRisposta;
-        let datiRisposta;
-        for(let x = 0; x<10; x++){
-        axios.get(this.indirizzo).then((result) => { //attenzione all'arrow function!!
 
-             codiceRisposta = `${result.status}/${result.statusText}`;
-             datiRisposta = result.data;
-
-            console.log("Ricevuta risposta", codiceRisposta, datiRisposta);
-
-            this.listaMail.push(datiRisposta.response);
         
-            });
-        }
     }
 
 }).mount('#app')
